deabb07dd2b4f4e915b00bb61e4e9bfd26f79a18
Zenahr Barzani <zenmatica@gmail.com>
2020-05-30 13:15:27 +0200

    updated App component

--- App/resources/js/components/App.js
+++ App/resources/js/components/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+
+    import React, { Component } from 'react'
     import ReactDOM from 'react-dom'
     import { BrowserRouter, Route, Switch } from 'react-router-dom'
     import Header from './Header'
+    import ProjectsList from './ProjectsList'
 
     class App extends Component {
       render () {
@@ -9,6 +11,9 @@ import React, { Component } from 'react'
           <BrowserRouter>
             <div>
               <Header />
+              <Switch>
+                <Route exact path='/' component={ProjectsList} />
+              </Switch>
             </div>
           </BrowserRouter>
         )
