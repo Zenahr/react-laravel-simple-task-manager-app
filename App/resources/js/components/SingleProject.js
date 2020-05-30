0370d9293c4651814ebff7f71877ca0003f0f84a
Zenahr Barzani <zenmatica@gmail.com>
2020-05-30 13:26:45 +0200

    added form for adding tasks to SingleProject Component

--- App/resources/js/components/SingleProject.js
+++ App/resources/js/components/SingleProject.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 
 class SingleProject extends Component {
     constructor(props) {
-    this.handleFieldChange = this.handleFieldChange.bind(this)
-    this.handleAddNewTask = this.handleAddNewTask.bind(this)
-    this.hasErrorFor = this.hasErrorFor.bind(this)
-    this.renderErrorFor = this.renderErrorFor.bind(this)
+        this.handleFieldChange = this.handleFieldChange.bind(this);
+        this.handleAddNewTask = this.handleAddNewTask.bind(this);
+        this.hasErrorFor = this.hasErrorFor.bind(this);
+        this.renderErrorFor = this.renderErrorFor.bind(this);
         super(props);
         this.state = {
             project: {},
@@ -22,51 +22,52 @@ class SingleProject extends Component {
             .then(response => history.push("/"));
     }
 
-    handleFieldChange (event) {
+    handleFieldChange(event) {
         this.setState({
-          title: event.target.value
-        })
-      }
-  
-      handleAddNewTask (event) {
-        event.preventDefault()
-  
+            title: event.target.value
+        });
+    }
+
+    handleAddNewTask(event) {
+        event.preventDefault();
+
         const task = {
-          title: this.state.title,
-          project_id: this.state.project.id
-        }
-  
-        axios.post('/api/tasks', task)
-          .then(response => {
-            // clear form input
-            this.setState({
-              title: ''
+            title: this.state.title,
+            project_id: this.state.project.id
+        };
+
+        axios
+            .post("/api/tasks", task)
+            .then(response => {
+                // clear form input
+                this.setState({
+                    title: ""
+                });
+                // add new task to list of tasks
+                this.setState(prevState => ({
+                    tasks: prevState.tasks.concat(response.data)
+                }));
             })
-            // add new task to list of tasks
-            this.setState(prevState => ({
-              tasks: prevState.tasks.concat(response.data)
-            }))
-          })
-          .catch(error => {
-            this.setState({
-              errors: error.response.data.errors
-            })
-          })
-      }
-  
-      hasErrorFor (field) {
-        return !!this.state.errors[field]
-      }
-  
-      renderErrorFor (field) {
+            .catch(error => {
+                this.setState({
+                    errors: error.response.data.errors
+                });
+            });
+    }
+
+    hasErrorFor(field) {
+        return !!this.state.errors[field];
+    }
+
+    renderErrorFor(field) {
         if (this.hasErrorFor(field)) {
-          return (
-            <span className='invalid-feedback'>
-              <strong>{this.state.errors[field][0]}</strong>
-            </span>
-          )
+            return (
+                <span className="invalid-feedback">
+                    <strong>{this.state.errors[field][0]}</strong>
+                </span>
+            );
         }
-      }
+    }
 
     componentDidMount() {
         const projectId = this.props.match.params.id;
@@ -99,6 +100,29 @@ class SingleProject extends Component {
                                 </button>
 
                                 <hr />
+
+                                <form onSubmit={this.handleAddNewTask}>
+                                    <div className="input-group">
+                                        <input
+                                            type="text"
+                                            name="title"
+                                            className={`form-control ${
+                                                this.hasErrorFor("title")
+                                                    ? "is-invalid"
+                                                    : ""
+                                            }`}
+                                            placeholder="Task title"
+                                            value={this.state.title}
+                                            onChange={this.handleFieldChange}
+                                        />
+                                        <div className="input-group-append">
+                                            <button className="btn btn-primary">
+                                                Add
+                                            </button>
+                                        </div>
+                                        {this.renderErrorFor("title")}
+                                    </div>
+                                </form>
 
                                 <ul className="list-group mt-3">
                                     {tasks.map(task => (
